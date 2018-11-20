e3831029803a9075aea4ef58652936ae33fe59fd
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-11-19 21:33:21 -0700

    Add api logic
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -10,8 +10,24 @@ app.get('/', function(req, res) {
   res.sendFile(path.resolve('views/index.html'));
 });
 
+app.get('/api/timestamp/:input', function(req, res) {
+  const { input } = req.params;
+
+  // convert numeric string to number
+  const date = input == +input ? new Date(+input) : new Date(input);
+
+  if (date instanceof Date && !isNaN(date)) {
+    // Valid date string or timestamp
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  } else {
+    // Invalid date
+    res.json({ error: 'Invalid Date' });
+  }
+});
+
 app.get('/api/timestamp', function(req, res) {
-  res.json({ greeting: 'HELLO API WORLD' });
+  const date = new Date();
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
 export default app;
